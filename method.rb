b9b095a66d89b7b017af8ade30f611006e179f79
hattori6789 <hattori.stdio@gmail.com>
2016-09-27 11:52:27 +0900

    change when section, add action initialize, add return after case

--- method.rb
+++ method.rb
@@ -1,20 +1,28 @@
 # 条件分岐、信号機
 # methodの学習、then 省略
 # 信号の色で、行動を判断する
+# IO#getsを使用してユーザーからの入力を引数にして
+# 信号の色を判断する、行動を返す関数
+# 行動の種類は、赤、黄、青と緑、それ以外、の四つ
 
+# 疑問
+# actionの初期化は必要？
+# returnはcase式のあとに抜けてから書くべき？
 
 def judgeAction(signalColor)
+	action = ""
 
 	case signalColor
 	when "赤"
-		return judge = "停止"
+		action = "停止"
 	when "黄"
-		return judge = "注意"
-	when "青", "緑"
-		return judge = "前進"
+		action = "注意"
+	when "緑", "青"
+		action = "前進"
 	else
-		return judge = "Unmatched color: 赤　黄　青（または緑）のいずれかを入力してください。"
+		action = "赤、黄、青、緑のいずれかを入力してください。"
 	end
+	return action
 
 end
 
